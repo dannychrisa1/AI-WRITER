bf6bd919c733cb47ccba65d8998d1967d0ceffa6
dannychrisa1 <dannychris.tk@gmail.com>
2024-08-29 11:33:46 +0100

    integrated  gemini api

--- src/app/_components/FormSection.tsx
+++ src/app/_components/FormSection.tsx
@@ -4,15 +4,28 @@ import { TEMPLATE } from "../_types/type";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { Loader2Icon } from "lucide-react";
 
 type selectedTemplateProp = {
     selectedTemplate: TEMPLATE
+    userFormInput: any
+    loading: boolean
 }
 
-export default function FormSection({ selectedTemplate }: selectedTemplateProp) {
-  
-    const onSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+export default function FormSection({ selectedTemplate, userFormInput, loading }: selectedTemplateProp) {
+
+
+    const [formData, setFormData] = useState<object>({})
+
+    const handleInputChange = (event: any) => {
+        const { name, value } = event.target;
+        setFormData({ ...formData, [name]: value })
+    }
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
+        userFormInput(formData)
+        // console.log(formData);
 
     }
 
@@ -23,16 +36,31 @@ export default function FormSection({ selectedTemplate }: selectedTemplateProp) 
             <p className="text-gray-500 text-sm">{selectedTemplate?.desc}</p>
             <form className="mt-6" onSubmit={onSubmit}>
                 {selectedTemplate?.form?.map((item, index) => (
-                    <div className="my-2 flex flex-col gap-2 mb-7">
+                    <div key={index} className="my-2 flex flex-col gap-2 mb-7">
                         <label className="font-bold">{item.label}</label>
                         {item.field == `input` ?
-                            <Input />
+                            <Input
+                                name={item.name}
+                                required={item.required}
+                                onChange={handleInputChange}
+                            />
                             : item.field == `textarea` ?
-                                <Textarea /> : ''
+                                <Textarea
+                                    name={item.name}
+                                    required={item.required}
+                                    onChange={handleInputChange}
+                                /> : ''
                         }
                     </div>
                 ))}
-                <Button type="submit" className="w-full py-6">Generate Content</Button>
+                <Button
+                    type="submit"
+                    className="w-full py-6"
+                    disabled={loading}
+                >
+                    {loading && <Loader2Icon className="animate-spin"/>}
+                    Generate Content
+                </Button>
             </form>
         </div>
     )
--- src/app/dashboard/content/[template-slug]/page.tsx
+++ src/app/dashboard/content/[template-slug]/page.tsx
@@ -1,8 +1,14 @@
-import { Metadata } from "next"
+"use client"
+
 import FormSection from "@/app/_components/FormSection"
 import OutputSection from "@/app/_components/OutputSection"
 import { Templates } from "@/app/_constants"
 import { TEMPLATE } from "@/app/_types/type"
+import { Button } from "@/components/ui/button"
+import { chatSession } from "@/utils/AImodal"
+import { ArrowLeft } from "lucide-react"
+import Link from "next/link"
+import { useState } from "react"
 
 
 type slugProps = {
@@ -11,32 +17,43 @@ type slugProps = {
     }
 }
 
-export const generateMetadata = async({params}:slugProps):Promise<Metadata> => {
-    const title = await new Promise(resolve => {
-        setTimeout(()=> {
-            resolve(`${params['template-slug']}`)
-        },100)
-    })
-    return{
-        title: `${title}`
-    }
-}
 
 
 export default function CreateContent({ params }: slugProps) {
-
     const selectedTemplate: TEMPLATE = Templates?.find((item) => item.slug === params['template-slug']) as TEMPLATE
+    const [loading, setLoading] = useState<boolean>(false)
+    const [aiOutput, setAiOutput] = useState<string>("")
+   
+    const GenerateAIContent = async(formData:any)=> {
+        setLoading(true)
+        const selectedPrompt = selectedTemplate?.aiPrompt;
+        const FinalAIPrompt = JSON.stringify(formData)+", "+selectedPrompt;
+        const result = await chatSession.sendMessage(FinalAIPrompt)
+        setAiOutput(result?.response.text())
+        console.log(result.response.text());
+        setLoading(false)
 
+    }
+    
     return (
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-10 p-5">
-            {/* Form Section */}
-            <FormSection selectedTemplate={selectedTemplate} />
+        <div className="p-10">
+            <Link href={"/dashboard"}>
+                <Button><ArrowLeft />Back</Button>
+            </Link>
 
-            {/* Output Section */}
-            <div className="col-span-2">
-                <OutputSection />
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-10 py-5">
+                {/* Form Section */}
+                <FormSection
+                    selectedTemplate={selectedTemplate}
+                    userFormInput={(v: any) => GenerateAIContent(v)}
+                    loading={loading}
+                />
+
+                {/* Output Section */}
+                <div className="col-span-2">
+                    <OutputSection aiOutput={aiOutput} />
+                </div>
             </div>
-
         </div>
     )
 }
